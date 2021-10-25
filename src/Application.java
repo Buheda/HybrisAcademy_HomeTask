364e6d31d88c8d41a47bb703ef0457506a1abfb8
Bugeda Alexey <feniragon@gmail.com>
2021-10-25 22:37:07 +0300

    init pom project with slf4j logger

---
+++ src/Application.java
@@ -0,0 +1,9 @@
+import the_collections_framework.Lecture25;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Lecture25.run();
+	}
+
+}
---
+++ src/the_collections_framework/Lecture25.java
@@ -0,0 +1,12 @@
+package the_collections_framework;
+
+import org.slf4j.LoggerFactory;
+
+public class Lecture25 {	
+	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(Lecture25.class);	 
+	
+	public static void run() {
+		logger.info("something");
+	}
+
+}
