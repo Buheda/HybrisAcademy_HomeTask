e8d89aca7929b6fabfeeae50cb065920ee81612b
Oleksii Buheda <oleksii.buheda@dxc.com>
2021-10-31 02:56:56 +0300

    Day1 -  Lambda - Lecture47

---
+++ src/com/udemy/ciklum/lambda_and_streamsAPI/lecture47_lambdaIntro/ALambdaInterface.java
@@ -0,0 +1,5 @@
+package com.udemy.ciklum.lambda_and_streamsAPI.lecture47_lambdaIntro;
+
+public interface ALambdaInterface {
+	public void execute();
+}
---
+++ src/com/udemy/ciklum/lambda_and_streamsAPI/lecture47_lambdaIntro/Application.java
@@ -0,0 +1,84 @@
+package com.udemy.ciklum.lambda_and_streamsAPI.lecture47_lambdaIntro;
+
+import org.apache.logging.log4j.Logger;
+
+import com.udemy.ciklum.util.LoggerUtil;
+
+public class Application {
+	private static Logger logger = LoggerUtil.getLogger("Lecture47");
+	
+	public static void main(String[] args) {
+		example1();
+		example2();
+		example3();
+		example4();
+		example5();
+		example6();
+	}
+	
+	static void humanWalker(Human human) {
+		human.walk();
+	}
+	
+	static void walker(Walkable walkableEntity) {
+		walkableEntity.walk();
+	}
+	
+	static void example1() {
+		logger.info("example1 - walk");
+		Human tom = new Human();
+		tom.walk();
+		
+		Robot walley = new Robot();
+		walley.walk();
+	}
+	
+	static void example2() {
+		logger.info("\nexample2 - humanWalker");
+		Human tom = new Human();
+		humanWalker(tom);
+		
+		Robot walley = new Robot();
+		walley.walk();
+	}
+	
+	static void example3() {
+		logger.info("\nexample3 - polymorphism");
+		Human tom = new Human();
+		walker(tom);
+		
+		Robot walley = new Robot();
+		walker(walley);
+	}
+	
+	static void example4() {
+		logger.info("\nexample4 - nested object");
+		
+		walker(new Walkable() {
+			@Override
+			public void walk() {
+				LoggerUtil.getLogger().trace("Custom object walking");
+			}
+		});
+	}
+	
+	static void example5() {
+		logger.info("\nexample5 - lambda");
+		
+		walker( () -> {
+			LoggerUtil.getLogger().trace("Custom object walking");
+		});
+	}
+	
+	static void example6() {
+		logger.info("\nexample6 - assign and pass");
+		
+		Walkable lambdaWalker = () -> {
+			LoggerUtil.getLogger().trace("Custom object walking");
+		};
+		
+		lambdaWalker.walk();
+		walker(lambdaWalker);
+		
+	}
+}
